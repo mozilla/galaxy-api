7486a863adecc88d04d2369dc211cac40c98eac6
Chris Van <cvan@mozilla.com>
2013-12-19 20:33:57 -0800

    return user/search results as a list (WIP)

--- views/user/search.js
+++ views/user/search.js
@@ -1,9 +1,12 @@
+var _ = require('lodash');
+
 var auth = require('../../lib/auth');
 var db = require('../../db');
 var user = require('../../lib/user');
 
 
 module.exports = function(server) {
+
     // Sample usage:
     // % curl 'http://localhost:5000/user/search?_user=ssatoken'
     server.get({
@@ -20,6 +23,10 @@ module.exports = function(server) {
             id: {
                 description: 'User ID to search for',
                 isRequired: false
+            },
+            q: {
+                description: 'Email/user ID/username to search for',
+                isRequired: false
             }
         }
     }, db.redisView(function(client, done, req, res) {
@@ -28,7 +35,8 @@ module.exports = function(server) {
 
         var lookup_email = DATA.email;
         var lookup_id = DATA.id;
-        var lookup = lookup_email || lookup_id;
+        var lookup_q = DATA.q;
+        var lookup = lookup_email || lookup_id || lookup_q;
 
         if (!lookup) {
             res.json(400, {error: 'Must provide either email or user ID'});
@@ -44,21 +52,46 @@ module.exports = function(server) {
             return;
         }
 
-        if (lookup_email) {
-            user.getUserFromEmail(client, lookup_email, function(err, obj) {
-                console.log('obj',obj)
+        // TODO: Allow searching by username.
+        // (i.e., if no results for email, look by username, etc.)
+        if (lookup_q) {
+            if (_.isNumber(lookup_q)) {
+                var lookup_id = lookup_q;
+            } else {
+                var lookup_email = lookup_q;
+            }
+
+            if (lookup_id) {
+                // TODO: Exclude yourself from the object list.
+                user.getPublicUserObjList(client, [lookup_id], function(objs) {
+                    done();
+                    res.json(objs);
+                });
+            }
+
+            user.getUserIDFromEmail(client, lookup, function(err, obj) {
                 if (err || !obj) {
-                    res.json(400, {error: err || 'bad_email'});
+                    res.json(400, {error: err || lookup_email ? 'bad_email' : 'bad_id'});
                     done();
                     return;
                 }
-                res.json(user.publicUserObj(obj));
-                done();
+                // TODO: Exclude yourself from the object list.
+                user.getPublicUserObjList(client, [obj], function(objs) {
+                    done();
+                    res.json(objs);
+                });
             });
-        } else if (lookup_id) {
-            user.getUserFromID(client, lookup_email, function(err, obj) {
+        } else {
+            // Return a single object.
+            (lookup_email ? user.getUserFromEmail : user.getUserFromID)(client, lookup, function(err, obj) {
                 if (err || !obj) {
-                    res.json(400, {error: err || 'bad_id'});
+                    res.json(400, {error: err || lookup_email ? 'bad_email' : 'bad_id'});
+                    done();
+                    return;
+                }
+                // TODO: Look up `id` from `getUserIDFromEmail` and compare.
+                if (obj.email === email) {
+                    res.json(400, {error: err || 'thats_you_silly'});
                     done();
                     return;
                 }
