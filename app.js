9c38aa7c9ed8adac05b0e5b659945dbd635254e1
Chris Van <cvan@mozilla.com>
2013-12-23 19:57:27 -0800

    restructure websocket code

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var settings = require('./settings_local.js');
 
 var db = require('./db');
-var server = require('./server');
+var serverHTTP = require('./server_http');
+var serverWS = require('./server_ws');
 
 var auth = require('./lib/auth');
 var user = require('./lib/user');
@@ -17,141 +18,15 @@ var user = require('./lib/user');
     'user/purchase',
     'user/search'
 ].forEach(function(view) {
-    require('./views/' + view)(server);
+    require('./views/' + view)(serverHTTP);
 });
 
-server.listen(process.env.PORT || 5000, function() {
-    console.log('%s listening at %s', server.name, server.url);
+var serverName = serverHTTP.name;
+
+serverHTTP.listen(process.env.PORT || 5000, function() {
+    console.log('%s HTTP server listening at %s', serverName, serverHTTP.url);
 });
 
-var WebSocket = require('ws');
-var WebSocketServer = WebSocket.Server;
-
-var wss = new WebSocketServer({server: server});
-console.log('websocket server created');
-
-wss.on('connection', function(ws) {
-    // create new redisClient.
-    var clientPub = db.redis();
-    var clientData = db.redis();
-    var authenticated = false;
-    var subscribed = false;
-
-    var user_ = new user.User(clientData);
-
-    function send(data) {
-        return ws.send(JSON.stringify(data));
-    }
-
-    ws.on('message', function(message) {
-        console.log('received: %s', message);
-
-        try {
-            message = JSON.parse(message);
-        } catch (e) {
-            // Ignore bad JSON bits.
-            return;
-        }
-
-        console.log('auth', user_.authenticated);
-        if (!user_.authenticated) {
-            if (message.type !== 'auth') {
-                // Ignore non-auth requests from the client until
-                // authentication has taken place.
-                send({type: 'error', error: 'not_authenticated'});
-                return;
-            }
-            var result = auth.verifySSA(message.token);
-            if (!result) {
-                send({type: 'error', error: 'bad_token'});
-                return;
-            }
-            user_.authenticate(result, function(err) {
-                if (err) {
-                    send({type: 'error', error: err});
-                } else {
-                    clientPub.subscribe('user:' + user_.get('id'));
-                    send({type: 'authenticated', id: user_.get('id')});
-                    // TODO: broadcast to friends that user is online.
-                }
-            });
-            return;
-        }
-        console.log('message', message)
-        switch (message.type) {
-            case 'playing':
-                user_.startPlaying(message.game, function(err) {
-                    if (!err) return;
-                    send({type: 'error', error: err});
-                });
-                // TODO: broadcast this to friends.
-                break;
-            case 'notPlaying':
-                user_.donePlaying();
-                break;
-            case 'score':
-                user_.updateLeaderboard(message.board, message.value, function(err) {
-                    if (!err) return;
-                    send({type: 'error', error: err});
-                });
-                break;
-            case 'postBlob':
-                // TODO: Throttle this.
-                if (typeof message.value !== 'string' || message.value > 1024) {
-                    send({type: 'error', error: 'invalid_blob'});
-                    return;
-                } else if (!user_.get('currentlyPlaying')) {
-                    send({type: 'error', error: 'not_playing_a_game'});
-                    return;
-                }
-                var blob;
-                try {
-                    blob = JSON.parse(message.value);
-                } catch(e) {
-                    send({type: 'error', error: 'invalid_blob'});
-                    return;
-                }
-                user_.isFriendsWith(message.recipient, function(resp) {
-                    if (!resp) {
-                        send({type: 'error', error: 'not_friends'});
-                        return;
-                    }
-                    clientData.sismember('gamePlaying:' + user_.get('currentlyPlaying'), message.recipient, function(err, resp) {
-                        if (err || !resp) {
-                            send({type: 'error', error: 'friend_not_playing'});
-                            return;
-                        }
-                        clientData.publish(
-                            'user:' + message.recipient,
-                            JSON.stringify({
-                                type: 'blob',
-                                blob: blob,
-                                from: user_.get('id')
-                            })
-                        );
-                    });
-                });
-                break;
-        }
-    });
-
-    ws.on('close', function() {
-        console.log('close');
-        if (user_.authenticated && subscribed) {
-            clientPub.unsubscribe();
-        }
-        user_.finish();
-        clientPub.end();
-        clientData.end();
-    });
-
-    clientPub.on('message', function(channel, message) {
-        if (!user_.authenticated) {
-            return;
-        }
-        ws.send(message);
-    });
-    clientPub.on('subscribe', function() {
-        subscribed = true;
-    });
+serverWS.listen(function(url) {
+    console.log('%s WebSocket server listening at %s', serverName, url);
 });
---
+++ server_http.js
@@ -0,0 +1,26 @@
+(function () {
+    var restify = require('restify');
+    var restifySwagger = require('node-restify-swagger');
+    var restifyValidation = require('node-restify-validation');
+
+    var server = restify.createServer({
+        name: 'galaxy',
+        version: '0.0.1'
+    });
+
+    server.use(restify.acceptParser(server.acceptable));
+    server.use(restify.bodyParser());
+    server.use(restify.CORS());
+    server.use(restify.gzipResponse());
+    server.use(restify.queryParser());
+    server.use(restifyValidation.validationPlugin({errorsAsArray: false}));
+
+    server.get(/\/static\/?.*/, restify.serveStatic({
+        directory: './static'
+    }));
+
+    restifySwagger.configure(server);
+    restifySwagger.loadRestifyRoutes();
+
+    module.exports = server;
+})();
