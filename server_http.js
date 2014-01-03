f567c7db6d24471be01d0a25b577c004bc5e85a5
Chris Van <cvan@mozilla.com>
2014-01-02 18:57:37 -0800

    remove unnecessary closure

--- server_http.js
+++ server_http.js
@@ -1,30 +1,28 @@
-(function () {
-    var restify = require('restify');
-    var restifySwagger = require('node-restify-swagger');
-    var restifyValidation = require('node-restify-validation');
+var restify = require('restify');
+var restifySwagger = require('node-restify-swagger');
+var restifyValidation = require('node-restify-validation');
 
-    var server = restify.createServer({
-        name: 'galaxy',
-        version: '0.0.1'
-    });
+var server = restify.createServer({
+    name: 'galaxy-api',
+    version: '0.0.1'
+});
 
-    server.use(restify.acceptParser(server.acceptable));
-    server.use(restify.bodyParser());
-    server.use(restify.CORS());
-    server.use(restify.gzipResponse());
-    server.use(restify.queryParser());
-    server.use(restifyValidation.validationPlugin({errorsAsArray: false}));
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+server.use(restify.gzipResponse());
+server.use(restify.queryParser());
+server.use(restifyValidation.validationPlugin({errorsAsArray: false}));
 
-    server.get(/\/static\/?.*/, restify.serveStatic({
-        directory: './static'
-    }));
+server.get(/\/static\/?.*/, restify.serveStatic({
+    directory: './static'
+}));
 
-    server.get(/\/data\/?.*/, restify.serveStatic({
-        directory: './data'
-    }));
+server.get(/\/data\/?.*/, restify.serveStatic({
+    directory: './data'
+}));
 
-    restifySwagger.configure(server);
-    restifySwagger.loadRestifyRoutes();
+restifySwagger.configure(server);
+restifySwagger.loadRestifyRoutes();
 
-    module.exports = server;
-})();
+module.exports = server;
