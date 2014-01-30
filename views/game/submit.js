865907959b577a78b79a360035e604066fb864db
Christopher Van <cvan@mozilla.com>
2014-01-29 16:27:14 -0800

    add artwork background

--- views/game/submit.js
+++ views/game/submit.js
@@ -43,6 +43,9 @@ module.exports = function(server) {
         var data = {
             app_url: POST.app_url,
             appcache_path: POST.appcache_path,
+            artwork: {
+                background: POST.artwork_background
+            },
             created: new Date(),
             default_locale: POST.default_locale,
             description: POST.description,
