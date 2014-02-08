b08608352c43e5f2cac373930c3671cb78258c6e
Charles Chong <CC.Charles.Chong@gmail.com>
2014-02-07 19:13:08 -0800

    added functionality to login 100 users

--- tests/db-prefill.js
+++ tests/db-prefill.js
@@ -1,10 +1,58 @@
-var http = require('http');
+var request = require('request');
+var settings = require('../settings');
+var settings_local = require('../settings_local');
 
 const API_ENDPOINT = 'http://localhost:5000';
+const PERSONA_ENDPOINT = 'http://localhost:9001';
 const USER_COUNT = 100;
 
 function createUsers() {
-    // do stuff
+    var SSATokens = [];
+
+    function createUser(email){
+        var req = request.post({
+            url: PERSONA_ENDPOINT+'/generate',
+            form: {
+                email: email
+            }
+        }, function(err, resp, body) {
+            if (err) {
+                console.log('Connection to assertion generator failed.', null);
+                return;
+            }
+            
+            var json_resp = JSON.parse(body);
+            login(email, json_resp.assertion);
+        });
+    };
+
+    function login(email, assertion){
+        var req = request.post({
+            url: API_ENDPOINT+'/user/login',
+            form: {
+                assertion: assertion,
+                audience: API_ENDPOINT
+            }
+        }, function(err, resp, body) {
+            if (err) {
+                console.log('Galaxy login failed.', null);
+                return;
+            }
+            
+            var json_resp = JSON.parse(body);
+            if (json_resp.error) {
+                console.log('Galaxy login failed:', json_resp.error);
+                return;
+            }
+
+            SSATokens[email] = json_resp.token;
+            console.log(json_resp)
+        });
+    };
+
+    for (var i = 0; i < USER_COUNT; i++){
+        createUser('test' + i + '@test.com')
+    }
 }
 
 function createGames() {
