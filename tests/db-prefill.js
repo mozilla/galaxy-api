f195b3bb8f26c7844e3417ee25e75e008f5bbcfe
Andrew Richardson <andrewr114@gmail.com>
2014-02-07 17:12:08 -0800

    Added skeleton for db prefill test

---
+++ tests/db-prefill.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+const API_ENDPOINT = 'http://localhost:5000';
+const USER_COUNT = 100;
+
+function createUsers() {
+    // do stuff
+}
+
+function createGames() {
+    // do stuff
+}
+
+createUsers();
+createGames();
