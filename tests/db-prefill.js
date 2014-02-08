b354d2d1f1032695aff172be8f048ab10c648027
Andrew Richardson <andrewr114@gmail.com>
2014-02-08 10:04:16 -0800

    Added working game submitting method

--- tests/db-prefill.js
+++ tests/db-prefill.js
@@ -1,4 +1,5 @@
-var http = require('http');
+var request = require('request');
+var _ = require('lodash');
 
 const API_ENDPOINT = 'http://localhost:5000';
 const USER_COUNT = 100;
@@ -8,7 +9,38 @@ function createUsers() {
 }
 
 function createGames() {
-    // do stuff
+    var default_params = {
+        icons: '128',
+        screenshots: 'yes'
+    };
+    var fake_games = [
+        {
+            name: 'Mario Broskis',
+            app_url: 'http://mario.broskis'
+        }, 
+        {
+            name: 'Halo 718',
+            app_url: 'http://halo.com'
+        },
+        {
+            name: 'Left 5 Dead',
+            app_url: 'http://dead.left'
+        }
+    ];
+
+    _.each(fake_games, function(game) {
+        request.post({
+            url: API_ENDPOINT + '/game/submit',
+            form: _.defaults(game, default_params)
+        }, function(err, resp, body) {
+            if (err) {
+                throw new Error(err);
+                return;
+            }
+
+            console.log('game! body:', body);
+        });
+    });
 }
 
 createUsers();
