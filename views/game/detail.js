daf64963ab220d5262e37bccae4f946e1179351f
Christopher Van <cvan@mozilla.com>
2014-02-09 09:13:13 -0800

    remove manifest (fixes #77)

--- views/game/detail.js
+++ views/game/detail.js
@@ -33,12 +33,4 @@ module.exports = function(server) {
             done();
         });
     }));
-
-    // TODO: Serve each manifest from a separate subdomain.
-    server.get({
-        url: '/manifest.html'
-    }, function(req, res) {
-        var app_url = req.url.split('?')[1];
-        res.send("<script>window.location = '" + app_url + "';</script>");
-    });
 };
