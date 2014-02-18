947dfe1ed904c5e2ec5f3d97782aa0ae1b7ea31e
Andrew Richardson <andrewr114@gmail.com>
2014-02-18 14:04:40 -0800

    Added an SSA verification plugin to restify that transforms the _user token, when present, to an email address, setting the _email param of the request object on success. If that step fails, it automatically returns a 403 bad_user response. This will help eliminate some boilerplate we've been using in pretty much every API endpoint.

--- server_http.js
+++ server_http.js
@@ -3,7 +3,7 @@ var restifySwagger = require('node-restify-swagger');
 var restifyValidation = require('node-restify-validation');
 
 var pkg = require('./package');
-
+var auth = require('./lib/auth');
 
 var server = restify.createServer({
     name: pkg.name,
@@ -16,6 +16,7 @@ server.use(restify.CORS());
 server.use(restify.gzipResponse());
 server.use(restify.queryParser());
 server.use(restifyValidation.validationPlugin({errorsAsArray: false}));
+server.use(auth.verifySSAPlugin());
 
 server.get(/\/static\/?.*/, restify.serveStatic({
     directory: './static'
