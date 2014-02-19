53267c771edc1de6cabd11e3eeac5343e666e0ff
Andrew Richardson <andrewr114@gmail.com>
2014-02-18 20:24:15 -0800

    Adopted userIDView for views that always need to fetch the user ID

--- views/user/profile.js
+++ views/user/profile.js
@@ -34,27 +34,16 @@ module.exports = function(server) {
                 isRequired: false,
             }
         }
-    }, db.redisView(function(client, done, req, res) {
+    }, user.userIDView(function(client, done, req, res, id) {
         var DATA = req.params;
-        var email = req._email;
-
-        user.getUserIDFromEmail(client, email, function(err, userID) {
-            if (err || !userID) {
-                res.json(500, {error: err || 'db_error'});
-                done();
-                return;
+        var dataToUpdate = _.pick(DATA, 'username', 'email');
+        user.updateUser(client, id, dataToUpdate, function(err, newUserData) {
+            if (err) {
+                res.json(500, {error: err});
+            } else {
+                res.json(202, {success: true});
             }
-
-            var dataToUpdate = _.pick(DATA, 'username', 'email');
-            user.updateUser(client, userID, dataToUpdate, function(err, newUserData) {
-                if (err) {
-                    res.json(500, {error: err});
-                } else {
-                    res.json(202, {success: true});
-                }
-                done();
-            });
+            done();
         });
-        
     }));
 };
