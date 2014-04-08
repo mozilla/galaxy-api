5a65ceb8a92e8fc2575b8a5cb27864a5768e3c58
scottmeng <blackridermkz@gmail.com>
2014-04-08 10:53:29 +0800

    reorder arguments

--- lua/insert_sorted.lua
+++ lua/insert_sorted.lua
@@ -1,25 +1,29 @@
-local count = redis.call("ZCARD", KEYS[1])
-local curPosition = redis.call("ZSCORE", KEYS[1], ARGV[1])
+local key = KEYS[1]
+local member = ARGV[2]
+local score = ARGV[1]
+
+local count = redis.call("ZCARD", key)
+local curPosition = redis.call("ZSCORE", key, member)
 
 -- if game is not yet featured
 if curPosition == false then
-	local items = redis.call("ZRANGEBYSCORE", KEYS[1], ARGV[2], count)
+	local items = redis.call("ZRANGEBYSCORE", key, score, count)
 	for i, item in ipairs(items) do
-		redis.call("ZINCRBY", KEYS[1], 1, item)
+		redis.call("ZINCRBY", key, 1, item)
 	end
-	return redis.call("ZADD", KEYS[1], ARGV[2], ARGV[1])
+	return redis.call("ZADD", key, score, member)
 -- if game is already featured
 else
-	if curPosition > ARGV[2] then
-		local items = redis.call("ZRANGEBYSCORE", KEYS[1], ARGV[2], curPosition - 1)
+	if curPosition > score then
+		local items = redis.call("ZRANGEBYSCORE", key, score, curPosition - 1)
 		for i, item in ipairs(items) do
-			redis.call("ZINCRBY", KEYS[1], 1, item)
+			redis.call("ZINCRBY", key, 1, item)
 		end
-	elseif curPosition < ARGV[2] then
-		local items = redis.call("ZRANGEBYSCORE", KEYS[1], curPosition + 1, ARGV[2])
+	elseif curPosition < score then
+		local items = redis.call("ZRANGEBYSCORE", key, curPosition + 1, score)
 		for i, item in ipairs(items) do
-			redis.call("ZINCRBY", KEYS[1], -1, item)
+			redis.call("ZINCRBY", key, -1, item)
 		end
 	end
-	return redis.call("ZINCRBY", KEYS[1], ARGV[2] - curPosition, ARGV[1])
+	return redis.call("ZINCRBY", key, score - curPosition, member)
 end
