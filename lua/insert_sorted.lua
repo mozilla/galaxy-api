04b80cf1ff8c3d7548b977faaeb8b8ddeaf3bf73
scottmeng <blackridermkz@gmail.com>
2014-04-14 13:15:31 +0800

    change tab to soft spaces

--- lua/insert_sorted.lua
+++ lua/insert_sorted.lua
@@ -7,31 +7,31 @@ local curPosition = redis.call("ZSCORE", key, member)
 
 -- if game is featured but no new position is given
 if (score == nil and curPosition ~= false) then
-	return 0
+  return 0
 else
-	score = count + 1
+  score = count + 1
 end
 
 -- if game is not yet featured
 if curPosition == false then
-	local items = redis.call("ZRANGEBYSCORE", key, score, count)
-	for i, item in ipairs(items) do
-		redis.call("ZINCRBY", key, 1, item)
-	end
-	return redis.call("ZADD", key, score, member)
+  local items = redis.call("ZRANGEBYSCORE", key, score, count)
+  for i, item in ipairs(items) do
+    redis.call("ZINCRBY", key, 1, item)
+  end
+  return redis.call("ZADD", key, score, member)
 
 -- if game is already featured
 else
-	if curPosition > score then
-		local items = redis.call("ZRANGEBYSCORE", key, score, curPosition - 1)
-		for i, item in ipairs(items) do
-			redis.call("ZINCRBY", key, 1, item)
-		end
-	elseif curPosition < score then
-		local items = redis.call("ZRANGEBYSCORE", key, curPosition + 1, score)
-		for i, item in ipairs(items) do
-			redis.call("ZINCRBY", key, -1, item)
-		end
-	end
-	return redis.call("ZINCRBY", key, score - curPosition, member)
+  if curPosition > score then
+    local items = redis.call("ZRANGEBYSCORE", key, score, curPosition - 1)
+    for i, item in ipairs(items) do
+      redis.call("ZINCRBY", key, 1, item)
+    end
+  elseif curPosition < score then
+    local items = redis.call("ZRANGEBYSCORE", key, curPosition + 1, score)
+    for i, item in ipairs(items) do
+      redis.call("ZINCRBY", key, -1, item)
+    end
+  end
+  return redis.call("ZINCRBY", key, score - curPosition, member)
 end
--- lua/remove_sorted.lua
+++ lua/remove_sorted.lua
@@ -6,12 +6,12 @@ local curPosition = redis.call("ZSCORE", key, member)
 
 -- if game is not yet featured
 if curPosition == false then
-	return 0
+  return 0
 -- if game is already featured
 else
-	local items = redis.call("ZRANGEBYSCORE", key, curPosition + 1, count)
-	for i, item in ipairs(items) do
-		redis.call("ZINCRBY", key, -1, item)
-	end
-	return redis.call("ZREM", key, member)
+  local items = redis.call("ZRANGEBYSCORE", key, curPosition + 1, count)
+  for i, item in ipairs(items) do
+    redis.call("ZINCRBY", key, -1, item)
+  end
+  return redis.call("ZREM", key, member)
 end
