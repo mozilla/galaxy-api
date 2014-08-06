2e9a5d93eeaa57f72ba5de6491703256e6f7e964
Christopher Van <cvan@mozilla.com>
2014-08-05 19:04:20 -0700

    use block comments for `redisClient` usage

--- db.js
+++ db.js
@@ -22,23 +22,25 @@ function scriptManager() {
 }
 exports.scriptManager = scriptManager;
 
-// lol
 var redisURL = url.parse(settings.REDIS_URL);
 
-// Examples:
-//
-//     export REDIS_URL='redis://[db-number[:password]@]host:port'
-//     export REDIS_URL='redis://[[:password]@]host:port/[db-number]'
-//
-// All fields after the scheme are optional and will default to
-// `localhost` on port `6379`, using database `0`.
-//
-// Default:
-//
-//     export REDIS_URL='redis://localhost:6379'
-//
-
-function redisClient() {
+/**
+ * Establish connection to Redis database.
+ *
+ * You must first define an environment variable, like so:
+ *
+ *     export REDIS_URL='redis://[db-number[:password]@]host:port'
+ *     export REDIS_URL='redis://[[:password]@]host:port/[db-number]'
+ *
+ * All fields after the scheme are optional and will default to
+ * `localhost` on port `6379`, using database `0`.
+ *
+ * This is a good default:
+ *
+ *     export REDIS_URL='redis://localhost:6379'
+ *
+ */
+ function redisClient() {
     var client = redis.createClient(parseInt(redisURL.port || '6379', 10),
         redisURL.hostname || 'localhost');
 
