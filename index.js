ac8a4d50a75c8306323113c938fe025a4a71c968
Christopher Van <cvan@mozilla.com>
2014-08-26 15:51:57 -0700

    forgo CLI in favour of external settings files

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ var ratelimit = require('koa-ratelimit');
 var responseTime = require('koa-response-time');
 var router = require('koa-router');
 
-var program = require('commander');
-
-// var application = require('./application');
 var db = require('./lib/db');
 var load = require('./lib/load');
+var settings = require('./settings');
 
 
 /**
@@ -22,24 +20,6 @@ var load = require('./lib/load');
 var env = process.env.NODE_ENV || 'development';
 
 var application = {};
-
-
-/**
- * Options.
- */
-// TODO: Load settings from `GALAXY_API_SETTINGS` file.
-program
-  .option('-H, --host <host>', 'specify the host [$HOST || 0.0.0.0]',
-    process.env.GALAXY_API_HOST || '0.0.0.0')
-  .option('-p, --port <port>', 'specify the port [$PORT || 4000]',
-    process.env.GALAXY_API_PORT || '4000')
-  .option('-b, --backlog <size>', 'specify the backlog size [511]',
-    process.env.GALAXY_API_BACKLOG || '511')
-  .option('-r, --ratelimit <n>', 'ratelimit requests [2500]',
-    process.env.GALAXY_API_RATELIMIT || '2500')
-  .option('-d, --ratelimit-duration <ms>', 'ratelimit duration [1h]',
-    process.env.GALAXY_API_RATELIMIT_DURATION || '1h')
-  .parse(process.argv);
 
 
 /**
@@ -72,11 +52,13 @@ app.use(compress());
 app.redis = db.redis();
 
 // Rate limiting.
-// app.use(ratelimit({
-//   max: ~~program.ratelimit
-//   duration: ~~program.ratelimitDuration
-//   db: app.redis
-// }));
+if (settings.RATELIMIT_ENABLED) {
+  app.use(ratelimit({
+    max: ~~settings.RATELIMIT_MAX,
+    duration: ~~settings.RATELIMIT_DURATION,
+    db: app.redis
+  }));
+}
 
 // Parse JSON bodies.
 app.use(body());
@@ -94,5 +76,5 @@ load(app, __dirname + '/api');
 /**
  * Listen.
  */
-app.listen(program.port, program.host, ~~program.backlog);
-console.log('Listening on %s:%s', program.host, program.port);
+app.listen(settings.PORT, settings.HOST, ~~settings.BACKLOG);
+console.log('Listening on %s:%s', settings.HOST, settings.PORT);
--- settings.js
+++ settings.js
@@ -6,7 +6,15 @@ if ('GALAXY_API_SETTINGS' in process.env) {
 
 exports.DEBUG = false;
 exports.ORIGIN = 'http://localhost/';
+
+exports.HOST = '0.0.0.0';
 exports.PORT = 4000;
+exports.BACKLOG_SIZE = 511;
+
+exports.RATELIMIT_ENABLED = false;
+exports.RATELIMIT_MAX = 2500;  // Max number of requests
+exports.RATELIMIT_DURATION = '1h';  // Duration of ratelimiting
+
 exports.REDIS_URL = 'redis://localhost:6379';
 exports.SECRET = 'a secret string';
 
