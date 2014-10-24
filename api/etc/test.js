527b0c3ca68090572fa287a8b0e33931ac2a53d8
Adrian Gaudebert <adrian@gaudebert.fr>
2014-10-24 20:03:56 +0900

    Working games and etc unit tests.

--- api/etc/test.js
+++ api/etc/test.js
@@ -3,73 +3,15 @@ var request = require('supertest');
 var api = require('../..');
 
 
-describe('GET /games', function () {
-  it('should respond with games', function (done) {
-    var app = api();
+describe('GET /', function () {
+  it('should respond with status and documentation', function (done) {
+    var app = api.app;
 
     request(app.listen())
-    .get('/games')
-    .expect(200)
-    .end(done);
-  });
-});
-
-
-describe('POST /games/', function () {
-  it('should respond with a success message', function (done) {
-    var app = api();
-
-    request(app.listen())
-    .post('/games')
-    .expect(200)
-    .end(done);
-  });
-});
-
-
-describe('GET /games/:slug', function () {
-  it('should respond with a single game', function (done) {
-    var app = api();
-
-    request(app.listen())
-    .get('/games/mario-bros')
-    .expect(200)
-    .end(done);
-  });
-});
-
-
-describe('PATCH /games/:slug', function () {
-  it('should respond with a success message', function (done) {
-    var app = api();
-
-    request(app.listen())
-    .patch('/games/mario-bros')
-    .expect(200)
-    .end(done);
-  });
-});
-
-
-describe('PUT /games/:slug', function () {
-  it('should respond with a success message', function (done) {
-    var app = api();
-
-    request(app.listen())
-    .put('/games/mario-bros')
-    .expect(200)
-    .end(done);
-  });
-});
-
-
-describe('DELETE /games/:slug', function () {
-  it('should respond with a success message', function (done) {
-    var app = api();
-
-    request(app.listen())
-    .delete('/games/mario-bros')
-    .expect(200)
-    .end(done);
+      .get('/')
+      .expect(/ok/)
+      .expect(/documentation/)
+      .expect(200)
+      .end(done);
   });
 });
