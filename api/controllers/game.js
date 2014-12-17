9b172192ab7f334893709f4b8e1d972187452e03
Christopher Van <cvan@mozilla.com>
2014-12-17 00:30:13 -0800

    send 404 when game not found

--- api/controllers/game.js
+++ api/controllers/game.js
@@ -3,23 +3,24 @@ var utils = require('../../lib/utils');
 
 
 module.exports = {
-  create: function (request) {
+  create: function (request, reply) {
     var game = new Game(request.payload);
 
     request.pg.client.query(
       'INSERT INTO games (description, game_url, name, slug, created) ' +
-      'VALUES ($1, $2, $3, $4, NOW())',
+      'VALUES ($1, $2, $3, $4, NOW()) RETURNING *',
       [game.description, game.game_url, game.name, game.slug],
       function (err, result) {
         if (err) {
           console.error(err);
         }
-        return result;
+        // TODO: Throw error if row couldn't be inserted.
+        reply(err || result.rows[0]);
     });
   },
   remove: function () {
   },
-  get: function (request) {
+  get: function (request, reply) {
     var query = (utils.isNumeric(request.params.idOrSlug) ?
       'SELECT * FROM games WHERE id = $1' :
       'SELECT * FROM games WHERE slug = $1'
@@ -30,7 +31,16 @@ module.exports = {
         if (err) {
           console.error(err);
         }
-        return result;
+        if (!result.rows.length) {
+          // TODO: Return proper JSON when row couldn't be seleced.
+          return reply({
+            statusCode: 404,
+            error: 'Not Found',
+            message: 'No game found'
+          }).code(404);
+        }
+        // TODO: Throw error for `err` when row couldn't get be selected.
+        reply(err || result.rows[0]);
     });
   },
   update: function () {
