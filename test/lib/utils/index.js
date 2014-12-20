cbc5342093b5d6620ddc217a5677008220b13855
Christopher Van <cvan@mozilla.com>
2014-12-19 16:11:24 -0800

    add tests for `lib.utils.promisify` (issue #309)

--- test/lib/utils/index.js
+++ test/lib/utils/index.js
@@ -1,6 +1,8 @@
 var Lab = require('lab');
 var Code = require('code');
 
+var Promise = require('es6-promise').Promise;
+
 var utils = require('../../../lib/utils');
 
 
@@ -71,6 +73,79 @@ lab.experiment('utils.stringifyObject', function () {
   lab.test('returns original argument for string', function (done) {
 
     Code.expect(utils.stringifyObject('lol')).to.equal('lol');
+    done();
+  });
+});
+
+
+lab.experiment('utils.promisify', function () {
+
+  lab.test('assignment returns function for callback', function (done) {
+
+    var cb = function () {};
+
+    Code.expect(utils.promisify(cb)).to.be.a.function();
+    done();
+  });
+
+  lab.test('assignment returns function for Promise', function (done) {
+
+    var prom = new Promise(function () {});
+
+    Code.expect(utils.promisify(prom)).to.be.a.function();
+    done();
+  });
+
+  lab.test('caller returns Promise for callback', function (done) {
+
+    var cb = function () {};
+    var promisified = utils.promisify(cb)();
+
+    Code.expect(promisified).to.not.equal(cb);
+    Code.expect(promisified).to.be.an.object();
+    Code.expect(promisified.then).to.be.a.function();
+    done();
+  });
+
+  lab.test('caller returns resolved Promise for success callback', function (done) {
+
+    var badError = new Error('bad');
+    var cb = function (callme) {
+      return callme(badError);
+    };
+    var promisified = utils.promisify(cb)();
+
+    promisified.then(function (value) {
+      Code.expect(value).to.not.exist();
+      done();
+    }, function (err) {
+      Code.expect(err).to.equal(badError);
+      done();
+    });
+  });
+
+  lab.test('caller returns rejected Promise for error callback', function (done) {
+
+    var cb = function (callme) {
+      return callme(null, 'good');
+    };
+    var promisified = utils.promisify(cb)();
+
+    promisified.then(function (value) {
+      Code.expect(value).to.equal('good');
+      done();
+    }, function (err) {
+      Code.expect(err).to.not.exist();
+      done();
+    });
+  });
+
+  lab.test('caller returns original Promise for Promise', function (done) {
+
+    var prom = new Promise(function () {});
+    var promisified = utils.promisify(prom)();
+
+    Code.expect(promisified).to.equal(prom);
     done();
   });
 });
