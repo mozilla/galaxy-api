8c67a1edde7081279c023203f957d7c10e3ada5c
Christopher Van <cvan@mozilla.com>
2014-12-22 15:52:42 -0800

    show all hapi validation errors in error responses

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ var routes = require('./api/routes');
 
 
 var server = new Hapi.Server();
-server.connection({host: settings.HOST, port: settings.PORT});
+server.connection({
+  host: settings.HOST,
+  port: settings.PORT,
+  routes: {
+    validate: {
+      options: {
+        abortEarly: false
+      }
+    }
+  }
+});
 routes(server);
 
 server.start(function () {
