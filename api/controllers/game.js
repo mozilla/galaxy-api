4db2576d338a021358b2c86e387b70390c8a2bd6
Christopher Van <cvan@mozilla.com>
2015-01-14 21:35:40 -0800

    simplify routes (fixes #268)

--- api/controllers/game.js
+++ api/controllers/game.js
@@ -1,38 +1,119 @@
 'use strict';
 
+var Joi = require('joi');
+
 var Game = require('../models/game');
+var utils = require('../../lib/utils');
 
 
-module.exports = {
-  all: function (request) {
-    return Game.objects.all(request.params)
+var gameSchema = {
+  // Game Description is optional.
+  description: Joi.string().example('Mario Bros. is a sweet adventure game.'),
+
+  // Game URL must start with `https://` or `http://`.
+  game_url: Joi.string().regex(/^https?:\/\//).required()
+               .example('http://nintendo.com/mario-bros/'),
+
+  // Game Name cannot be longer than 150 characters long.
+  name: Joi.string().max(150).required()
+           .example('Mario Bros.'),
+
+  // Game Slug cannot be all digits, all underscores, or all hyphens
+  // and must contain only letters, numbers, underscores, and hyphens.
+  // TODO: Throw an error if `slug` is already taken.
+  slug: Joi.string().regex(/^(?!\d*$)(?!_*$)(?!-*$)[\w-]+$/).required()
+  .example('mario-bros')
+};
+
+
+exports.all = {
+  handler: function (request, reply) {
+
+    Game.objects.all(request.params)
     .then(function (games) {
-      return games.map(Game.getPublicObj);
+
+      reply(games.map(Game.getPublicObj));
+    })
+    .catch(function (err) {
+
+      reply(utils.returnError(err));
     });
+  }
+};
+
+
+exports.create = {
+  validate: {
+    payload: gameSchema
   },
-  create: function (request) {
-    return Game.objects.create(request.payload)
+  handler: function (request, reply) {
+
+    Game.objects.create(request.payload)
     .then(function (res) {
-      return {
-        body: Game.getPublicObj(res.body),
-        uri: res.uri
-      };
+
+      var body = Game.getPublicObj(res.body);
+      var uri = res.uri;
+
+      reply(body).created(uri);
+    })
+    .catch(function (err) {
+
+      reply(utils.returnError(err));
     });
+  }
+};
+
+
+exports.get = {
+  handler: function (request, reply) {
+
+    Game.objects.get(request.params)
+    .then(function (game) {
+
+      reply(Game.getPublicObj(game));
+    })
+    .catch(function (err) {
+
+      reply(utils.returnError(err));
+    });
+  }
+};
+
+
+exports.remove = {
+  handler: function (request, reply) {
+
+    Game.objects.remove(request.params)
+    .then(reply)
+    .catch(function (err) {
+
+      reply(utils.returnError(err));
+    });
+  }
+};
+
+
+exports.update = {
+  validate: {
+    payload: gameSchema
   },
-  get: function (request) {
-    return Game.objects.get(request.params)
-    .then(Game.getPublicObj);
-  },
-  remove: function (request) {
-    return Game.objects.remove(request.params);
-  },
-  update: function (request) {
-    return Game.objects.update(request.params, request.payload)
+  handler: function (request, reply) {
+
+    Game.objects.update(request.params, request.payload)
     .then(function (res) {
-      return {
-        body: Game.getPublicObj(res.body),
-        uri: res.uri
-      };
+
+      var body = Game.getPublicObj(res.body);
+      var uri = res.uri;
+
+      if (uri) {
+        return reply(body).redirect(uri);
+      }
+
+      reply(body);
+    })
+    .catch(function (err) {
+
+      reply(utils.returnError(err));
     });
   }
 };
